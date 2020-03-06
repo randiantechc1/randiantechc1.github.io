c1041dca3c9104b0a06cb77fae56e30a5daf5104
Juan Carlos Cancela <bak@Juans-MacBook-Pro-3.local>
2020-03-06 01:44:27 -0300

    initial commit

---
+++ precache-manifest.eba1d3175a91799bb78e2f00100134fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d24fb7497d2deea6c1c2c5d376c40b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d45386ef8d6078c8b73",
+    "url": "/static/css/main.f4cf7d1f.chunk.css"
+  },
+  {
+    "revision": "0f9be4782459c75411cb",
+    "url": "/static/js/2.90f87e7f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.90f87e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d45386ef8d6078c8b73",
+    "url": "/static/js/main.fbec1cae.chunk.js"
+  },
+  {
+    "revision": "455bddb9160f282f0a6c",
+    "url": "/static/js/runtime-main.2139b927.js"
+  }
+]);
